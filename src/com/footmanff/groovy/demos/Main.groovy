a9130874932b07f5ff7b7730bb80ae7df7252964
footmanff <footman265@gmail.com>
2018-11-11 17:29:41 +0800

    Initial commit

---
+++ src/com/footmanff/groovy/demos/Main.groovy
@@ -0,0 +1,12 @@
+package com.footmanff.groovy.demos
+
+/**
+ * @author footmanff on 2018/11/11.
+ */
+class Main {
+
+    static void main(String[] args) {
+        printf "hello world"    
+    }
+    
+}
